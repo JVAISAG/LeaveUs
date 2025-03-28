661869c9fd454d81e44d0010eaa74b52bb2423eb
Allen George <81978331+CyberPidgi@users.noreply.github.com>
2025-03-28 21:26:46 +0530

    added basic skeleton for student data fetching

--- backend/router/studentRoute.js
+++ backend/router/studentRoute.js
@@ -1,1 +1,26 @@
-import { express } from "express";
+const express = require("express");
+const Student = require("../models/students");
+
+const router = express.Router();
+
+router.post("/", async (request, response) => {});
+
+router.get("/", async (request, response) => {
+  return response.status(200).send("student route is working");
+});
+
+router.get("/all", async (request, response) => {
+  try {
+    const students = await Student.find({});
+    console.log("getting all students");
+    console.log(students)
+    return response.status(200).json({
+      count: students.count,
+      data: students,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
+// import studentRoute from './router/studentRoute.js'
+
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const studentRoute = require("./router/studentRoute.js")
 
 // Connect to Database
 connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.get('/', async (request, response) => {
+  return response.status(200).send("Backend is working!")
+})
+
+app.use('/student', studentRoute)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
