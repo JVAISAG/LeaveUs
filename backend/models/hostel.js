adc471e0bb8f3f02a9667008ffea8919374473c9
ManishAnand05 <manishanand557@gmail.com>
2025-03-30 10:53:43 +0530

    Add authentication

---
+++ backend/models/hostel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const HostelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,  
+        trim: true
+    },
+    wardens: [
+        {
+            type: String,
+            required: true,
+            trim: true
+        }
+    ],
+    chiefWarden: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, { timestamps: true });  
+
+const Hostel = mongoose.model("Hostel", HostelSchema);
+module.exports = Hostel;
