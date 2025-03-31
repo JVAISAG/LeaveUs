fbf01c78a706ddb48be3787954c97cf660a2192d
ManishAnand05 <106062529+ManishAnand05@users.noreply.github.com>
2025-03-31 20:56:18 +0530

    Update hostel.js

--- backend/models/hostel.js
+++ backend/models/hostel.js
@@ -21,5 +21,5 @@ const HostelSchema = new mongoose.Schema({
     }
 }, { timestamps: true });  
 
-const Hostel = mongoose.model("Hostel", HostelSchema);
+const Hostel = mongoose.model(hostels", HostelSchema);
 module.exports = Hostel;
