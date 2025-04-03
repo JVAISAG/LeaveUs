213e5ef76c43ab542c441347e774314e3551000d
Allen George <81978331+CyberPidgi@users.noreply.github.com>
2025-04-03 14:45:03 +0530

    added route for HOD and Dean approval and leave fetching

--- backend/router/leaveRoute.js
+++ backend/router/leaveRoute.js
@@ -9,20 +9,14 @@ const { ROLE, APPROVAL_STATUS, STATUS } = require("../../constants/approvals");
 
 const router = express.Router();
 const approvalOrder = [
-  "Pending",
-  "AdvisorApproved",
-  "WardenApproved",
-  "HODApproved",
-  "DeanApproved",
-  "Accepted",
-]
+  APPROVAL_STATUS.PENDING,
+  APPROVAL_STATUS.ADVISOR_APPROVED,
+  APPROVAL_STATUS.WARDEN_APPROVED,
+  APPROVAL_STATUS.HOD_APPROVED,
+  APPROVAL_STATUS.DEAN_APPROVED,
+];
 
-const approvalRoles = [
-  ROLE.ADVISOR,
-  ROLE.WARDEN,
-  ROLE.HOD,
-  ROLE.DEAN,
-]
+const approvalRoles = [ROLE.ADVISOR, ROLE.WARDEN, ROLE.HOD, ROLE.DEAN];
 
 const validateAdvisor = async (facultyId, studentId) => {
   const advisor = (await Student.findById(studentId)).facultyAdvisor;
@@ -38,7 +32,7 @@ const validateAdvisor = async (facultyId, studentId) => {
   }
 
   return true;
-}
+};
 
 const validateWarden = async (facultyId, hostelId) => {
   const hostel = await Hostel.findById(hostelId);
@@ -53,9 +47,7 @@ const validateWarden = async (facultyId, hostelId) => {
   }
 
   return true;
-
-}
-
+};
 
 router.get("/", async (request, response) => {
   return response.status(200).send("Leave route is working");
@@ -63,10 +55,12 @@ router.get("/", async (request, response) => {
 
 router.post("/new", async (request, response) => {
   try {
-
     request.body.startDate = new Date(request.body.startDate);
     request.body.endDate = new Date(request.body.endDate);
-    request.body.workingdays = Math.ceil((request.body.endDate - request.body.startDate) / (1000 * 60 * 60 * 24)) + 1;
+    request.body.workingdays =
+      Math.ceil(
+        (request.body.endDate - request.body.startDate) / (1000 * 60 * 60 * 24)
+      ) + 1;
 
     // const hostelname = request.body.hostelName;
 
@@ -90,48 +84,67 @@ router.post("/new", async (request, response) => {
   }
 });
 
-router.post('/:id/approve', async (request, response) => {
+router.post("/:id/approve", async (request, response) => {
   try {
     const { id } = request.params;
     let { status, facultyId } = request.body;
 
     facultyId = mongoose.Types.ObjectId.createFromHexString(facultyId);
 
-    
     if (status === STATUS.REJECTED) {
-      return response.status(400).json({ message: "Using /approve for rejecting application. Use /reject instead." });
+      return response
+        .status(400)
+        .json({
+          message:
+            "Using /approve for rejecting application. Use /reject instead.",
+        });
     }
-    
+
     const leave = await Leave.findById(id);
     if (!leave) {
       return response.status(404).json({ message: "Leave not found" });
     }
-    
-    console.log(leave.finalApproval)
+
+    console.log(leave.finalApproval);
     if (leave.finalApproval !== STATUS.PENDING) {
-      return response.status(400).json({ message: "Leave request already processed" });
+      return response
+        .status(400)
+        .json({ message: "Leave request already processed" });
     }
     const { nextApproverRole } = leave;
+
+    const faculty = Faculty.findById(facultyId);
+    if (!faculty) {
+      return response
+        .status(403)
+        .json({ message: "You are not authorized to approve this leave" });
+    }
 
     if (nextApproverRole === ROLE.ADVISOR) {
       const isValid = await validateAdvisor(facultyId, leave.studentId);
       status = APPROVAL_STATUS.ADVISOR_APPROVED;
       if (!isValid) {
-        return response.status(403).json({ message: "You are not authorized to approve this leave" });
+        return response
+          .status(403)
+          .json({ message: "You are not authorized to approve this leave" });
       }
-    } 
-    else if (nextApproverRole === ROLE.WARDEN) {
+    } else if (nextApproverRole === ROLE.WARDEN) {
       const isValid = await validateWarden(facultyId, leave.hostelId);
       if (!isValid) {
-        return response.status(403).json({ message: "You are not authorized to approve this leave" });
+        return response
+          .status(403)
+          .json({ message: "You are not authorized to approve this leave" });
       }
       status = APPROVAL_STATUS.WARDEN_APPROVED;
+    } else if (nextApproverRole === ROLE.HOD) {
+      status = APPROVAL_STATUS.HOD_APPROVED;
+    } else if (nextApproverRole === ROLE.DEAN) {
+      status = APPROVAL_STATUS.DEAN_APPROVED;
+    } else {
+      return response
+        .status(403)
+        .json({ message: "Invalid role provided. Not Authorized." });
     }
-
-    else {
-      return response.status(403).json({ message: "Testing till warden right now" });
-    }
-      
 
     // check if the faculty is the warden of the hostel
     // const hostel = await Hostel.findOne({ _id: leave.hostelId, wardens: facultyId });
@@ -141,13 +154,29 @@ router.post('/:id/approve', async (request, response) => {
 
     // update the approval status
     console.log(status);
+
+    let setQuery = {};
+    if (
+      (leave.workingdays <= 2 && status == APPROVAL_STATUS.WARDEN_APPROVED) ||
+      status === APPROVAL_STATUS.DEAN_APPROVED
+    ) {
+      setQuery = {
+        status: APPROVAL_STATUS.ACCEPTED,
+        finalApproval: STATUS.APPROVED,
+        nextApproverRole: STATUS.APPROVED,
+      };
+    } else {
+      setQuery = {
+        status: status,
+        nextApproverRole:
+          approvalRoles[approvalRoles.indexOf(nextApproverRole) + 1],
+      };
+    }
+
     await Leave.updateOne(
       { _id: leave._id },
       {
-        $set: {
-          status: status,
-          nextApproverRole: approvalRoles[approvalRoles.indexOf(nextApproverRole) + 1],
-        },
+        $set: setQuery,
       }
     );
 
@@ -156,13 +185,15 @@ router.post('/:id/approve', async (request, response) => {
       leave: leave,
     });
   } catch (error) {
-    console.log("Error occurred at leave route POST leaveform/:id/approve", error.message);
+    console.log(
+      "Error occurred at leave route POST leaveform/:id/approve",
+      error.message
+    );
     return response.status(400).send("Something went wrong");
   }
 });
 
-
-router.post('/:id/reject', async (request, response) => {
+router.post("/:id/reject", async (request, response) => {
   try {
     const { id } = request.params;
     let { status, facultyId } = request.body;
@@ -170,7 +201,12 @@ router.post('/:id/reject', async (request, response) => {
     facultyId = mongoose.Types.ObjectId.createFromHexString(facultyId);
 
     if (status === STATUS.APPROVED) {
-      return response.status(400).json({ message: "Using /reject for approving application. Use /approve instead." });
+      return response
+        .status(400)
+        .json({
+          message:
+            "Using /reject for approving application. Use /approve instead.",
+        });
     }
 
     const leave = await Leave.findById(id);
@@ -179,7 +215,9 @@ router.post('/:id/reject', async (request, response) => {
     }
 
     if (leave.finalApproval !== STATUS.PENDING) {
-      return response.status(400).json({ message: "Leave request already processed" });
+      return response
+        .status(400)
+        .json({ message: "Leave request already processed" });
     }
 
     await Leave.updateOne(
@@ -198,11 +236,12 @@ router.post('/:id/reject', async (request, response) => {
       leave: leave,
     });
   } catch (error) {
-    console.log("Error occurred at leave route POST leaveform/:id/reject", error.message);
+    console.log(
+      "Error occurred at leave route POST leaveform/:id/reject",
+      error.message
+    );
     return response.status(400).send("Something went wrong");
-    
   }
 });
-
 
 module.exports = router;
